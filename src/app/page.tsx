6c4d975f3fd28e05b1aea961bfc9c954d9ea64f8
Carlos Simon Camacho <caloycamacho02@gmail.com>
2025-07-04 07:03:13 +0800

    feat: update font styles and add Satoshi font family

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <div className="font-oswald text-4xl">Hello World</div>
+      <div className="font-montserrat text-4xl">Hello World</div>
+      <div className="font-satoshi text-4xl">Hello World</div>
+    </div>
+  );
 };
 
 export default Home;
