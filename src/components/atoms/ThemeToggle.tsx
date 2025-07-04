5c0352f42d231f0997b2caeac032cb2224ee275a
Carlos Simon Camacho <caloycamacho02@gmail.com>
2025-07-04 15:23:35 +0800

    feat: update ThemeToggle component to use Sparkle icon instead of Sun

--- src/components/atoms/ThemeToggle.tsx
+++ src/components/atoms/ThemeToggle.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Moon, Sun } from "lucide-react";
+import { Moon, Sparkle } from "lucide-react";
 import { useTheme } from "next-themes";
 
 const ThemeToggle = () => {
@@ -11,8 +11,8 @@ const ThemeToggle = () => {
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
       className="relative size-10"
     >
-      <Sun className="absolute h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-      <Moon className="h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+      <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+      <Sparkle className="h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
       <span className="sr-only">Toggle theme</span>
     </button>
   );
